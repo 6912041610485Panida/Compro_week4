7e33a50d04b5835c9b6fc1811b1a1bc0832445ba
69120416100485-Panida <s6912041610485@email.kmutnb.ac.th>
2026-07-31 07:18:48 +0000

    นกที่ปวดท้อวมาเรียนไม่ได้

---
+++ week_4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+                                 //ประกาศตัวแปรเก็บข้อมูลส่วนบุคคล
+    char name[30];               //ชื่อ
+    int age;                     //อายุ
+    float height;                //ส่วนสูง
+
+                                 //ประกาศตัวแปรเก็บข้อมูลน้ำดื่ม
+    char drink[30];              //เครื่องดื่ม
+    float price;                   //ราคา
+    char size;                   //ไซส์
+
+    scanf("%s %d %f", name, &age, &height);    //รับข้อมูลบรรทัดที่1
+    scanf("%s %f %c", drink, &price, &size);   //รับข้อมูลบรรทัดที่2
+
+    //แสดงข้อมูลส่วนบุคคล
+    printf("%s is %d years old and is %.1f cm tall.\n", name, age, height);
+
+    //แสดงข้อมูลเครื่องดื่ม
+    printf("Favorite drink %s costs %.2f baht and has size %c.\n", drink, price, size);
+
+    return 0;                     //รับข้อมูลบรรทัดที่1
+}
